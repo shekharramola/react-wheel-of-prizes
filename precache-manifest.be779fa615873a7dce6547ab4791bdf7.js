5f9fbd4c0d9c08ab5acc870355f3da2d65b1c645
Shekhar Ramola <shekhar.ramola.cse@gmail.com>
2020-05-26 22:17:20 +0530

    Updates

---
+++ precache-manifest.be779fa615873a7dce6547ab4791bdf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96766c2e463c5291c8f4cbb329011a9d",
+    "url": "/react-wheel-of-prizes/index.html"
+  },
+  {
+    "revision": "6604a3044fbcfc628402",
+    "url": "/react-wheel-of-prizes/static/css/main.d832607b.chunk.css"
+  },
+  {
+    "revision": "d2fbbf3adb4f872a2a56",
+    "url": "/react-wheel-of-prizes/static/js/2.799c7851.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-wheel-of-prizes/static/js/2.799c7851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6604a3044fbcfc628402",
+    "url": "/react-wheel-of-prizes/static/js/main.d32713b5.chunk.js"
+  },
+  {
+    "revision": "66d4f5428ad38e089415",
+    "url": "/react-wheel-of-prizes/static/js/runtime-main.dad76a84.js"
+  }
+]);
