01eea87cacbe55073a892dc8fc2de52cf15379f4
Shekhar Ramola <shekhar.ramola.cse@gmail.com>
2020-09-20 12:54:22 +0530

    solved es lint error. changed variable to camelCase

--- example/src/App.js
+++ example/src/App.js
@@ -1,35 +1,59 @@
 import React from 'react'
 
-import { WheelComponent } from 'react-wheel-of-prizes'
+import WheelComponent from 'react-wheel-of-prizes'
 import 'react-wheel-of-prizes/dist/index.css'
 
 const App = () => {
-  const segments = ['better luck next time', 'won 70', 'won 10','better luck next time', 'won 2', 'won uber pass', 'better luck next time', 'won a voucher'];
-  const seg_colors = [
-    "#EE4040",
-    "#F0CF50",
-    "#815CD1",
-    "#3DA5E0",
-    "#34A24F",
-    "#F9AA1F",
-    "#EC3F3F",
-    "#FF9000",
-  ];
+  const segments = [
+    'better luck next time',
+    'won 70',
+    'won 10',
+    'better luck next time',
+    'won 2',
+    'won uber pass',
+    'better luck next time',
+    'won a voucher',
+    'won 70',
+    'won 10',
+    'better luck next time',
+    'won 2',
+    'won uber pass',
+    'better luck next time',
+    'won a voucher'
+  ]
+  const segColors = [
+    '#EE4040',
+    '#F0CF50',
+    '#815CD1',
+    '#3DA5E0',
+    '#34A24F',
+    '#F9AA1F',
+    '#EC3F3F',
+    '#FF9000',
+    '#F0CF50',
+    '#815CD1',
+    '#3DA5E0',
+    '#34A24F',
+    '#F9AA1F',
+    '#EC3F3F',
+    '#FF9000'
+  ]
   const onFinished = (winner) => {
-    console.log(winner);
+    console.log(winner)
   }
-  return <React.Fragment>
-
-<WheelComponent
-  segments = {segments}
-  seg_colors = {seg_colors} 
-  winning_segment= {segments[3]}
-  onFinished={(winner)=>onFinished(winner)}
-  primaryColor='black'
-  contrastColor='white'
-  buttonText='Spin'/>
-
-  </React.Fragment>
+  return (
+    <React.Fragment>
+      <WheelComponent
+        segments={segments}
+        segColors={segColors}
+        winningSegment={segments[3]}
+        onFinished={(winner) => onFinished(winner)}
+        primaryColor='black'
+        contrastColor='white'
+        buttonText='Spin'
+      />
+    </React.Fragment>
+  )
 }
 
 export default App
