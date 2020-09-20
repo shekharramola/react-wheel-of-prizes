dd61a519265b76bd98d2f5047e654a9963f4dd93
Shekhar Ramola <shekhar.ramola.cse@gmail.com>
2020-09-20 12:53:36 +0530

    Added default export

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 import React, { useEffect, useState } from 'react'
-import styles from './styles.module.css'
 
-export const WheelComponent = ({
+ const WheelComponent = ({
   segments,
-  seg_colors,
-  winning_segment,
+  segColors,
+  winningSegment,
   onFinished,
   primaryColor,
   contrastColor,
   buttonText
 }) => {
-  let current_segment = ''
-  let isStarted = false;
+  let currentSegment = ''
+  let isStarted = false
   const [isFinished, setFinished] = useState(false)
   let timerHandle = 0
-  let timerDelay = segments.length
+  const timerDelay = segments.length
   let angleCurrent = 0
   let angleDelta = 0
-  let size = 290
+  const size = 290
   let canvasContext = null
-  let maxSpeed = Math.PI / (segments.length)
-  let upTime = segments.length*100;
-  let downTime = segments.length*1000;
+  let maxSpeed = Math.PI / `${segments.length}`
+  const upTime = segments.length * 100
+  const downTime = segments.length * 1000
   let spinStart = 0
   let frames = 0
-  let centerX = 300
-  let centerY = 300
+  const centerX = 300
+  const centerY = 300
   useEffect(() => {
-    wheelInit();
+    wheelInit()
     setTimeout(() => {
       window.scrollTo(0, 1)
     }, 0)
@@ -36,7 +35,7 @@ export const WheelComponent = ({
     initCanvas()
     wheelDraw()
   }
-  
+
   const initCanvas = () => {
     let canvas = document.getElementById('canvas')
     if (navigator.appVersion.indexOf('MSIE') !== -1) {
@@ -50,11 +49,11 @@ export const WheelComponent = ({
     canvasContext = canvas.getContext('2d')
   }
   const spin = () => {
-    isStarted = true;
+    isStarted = true
     if (timerHandle === 0) {
       spinStart = new Date().getTime()
       // maxSpeed = Math.PI / ((segments.length*2) + Math.random())
-      maxSpeed = Math.PI / ((segments.length))
+      maxSpeed = Math.PI / segments.length
       frames = 0
       timerHandle = setInterval(onTimerTick, timerDelay)
     }
@@ -62,20 +61,19 @@ export const WheelComponent = ({
   const onTimerTick = () => {
     frames++
     draw()
-    let duration = new Date().getTime() - spinStart
+    const duration = new Date().getTime() - spinStart
     let progress = 0
     let finished = false
     if (duration < upTime) {
       progress = duration / upTime
       angleDelta = maxSpeed * Math.sin((progress * Math.PI) / 2)
     } else {
-      if (winning_segment) {
-        if (current_segment === winning_segment && frames > segments.length*20) {
+      if (winningSegment) {
+        if (currentSegment === winningSegment && frames > segments.length) {
+          progress = duration / upTime
           angleDelta =
-          maxSpeed * Math.sin((progress * Math.PI) / 2 + Math.PI / 2)
+            maxSpeed * Math.sin((progress * Math.PI) / 2 + Math.PI / 2)
           progress = 1
-          
-          
         } else {
           progress = duration / downTime
           angleDelta =
@@ -91,11 +89,11 @@ export const WheelComponent = ({
     angleCurrent += angleDelta
     while (angleCurrent >= Math.PI * 2) angleCurrent -= Math.PI * 2
     if (finished) {
-      setFinished(true);
-      onFinished(current_segment);
-      clearInterval(timerHandle);
-      timerHandle = 0;
-      angleDelta = 0;
+      setFinished(true)
+      onFinished(currentSegment)
+      clearInterval(timerHandle)
+      timerHandle = 0
+      angleDelta = 0
     }
   }
 
@@ -112,15 +110,15 @@ export const WheelComponent = ({
   }
 
   const drawSegment = (key, lastAngle, angle) => {
-    let ctx = canvasContext
-    let value = segments[key]
+    const ctx = canvasContext
+    const value = segments[key]
     ctx.save()
     ctx.beginPath()
     ctx.moveTo(centerX, centerY)
     ctx.arc(centerX, centerY, size, lastAngle, angle, false)
     ctx.lineTo(centerX, centerY)
     ctx.closePath()
-    ctx.fillStyle = seg_colors[key]
+    ctx.fillStyle = segColors[key]
     ctx.fill()
     ctx.stroke()
     ctx.save()
@@ -133,51 +131,47 @@ export const WheelComponent = ({
   }
 
   const drawWheel = () => {
-    let ctx = canvasContext
+    const ctx = canvasContext
     let lastAngle = angleCurrent
-    let len = segments.length
-    let PI2 = Math.PI * 2
+    const len = segments.length
+    const PI2 = Math.PI * 2
     ctx.lineWidth = 1
     ctx.strokeStyle = primaryColor || 'black'
     ctx.textBaseline = 'middle'
     ctx.textAlign = 'center'
     ctx.font = '1em proxima-nova'
     for (let i = 1; i <= len; i++) {
-      let angle = PI2 * (i / len) + angleCurrent
+      const angle = PI2 * (i / len) + angleCurrent
       drawSegment(i - 1, lastAngle, angle)
       lastAngle = angle
     }
 
-     // Draw a center circle
-     ctx.beginPath();
-     ctx.arc(centerX, centerY, 50, 0, PI2, false);
-     ctx.closePath();
-     ctx.fillStyle =primaryColor || "black";
-     ctx.lineWidth = 10;
-     ctx.strokeStyle = contrastColor || "white";
-     ctx.fill();
-     ctx.font = "bold 1em proxima-nova";
-     ctx.fillStyle = contrastColor || 'white';
-     ctx.textAlign = "center";
-     ctx.fillText(buttonText || 'Spin',
-       centerX,
-       centerY + 3
-     );
-     ctx.stroke();
- 
-     // Draw outer circle
-     ctx.beginPath();
-     ctx.arc(centerX, centerY, size, 0, PI2, false);
-     ctx.closePath();
- 
-     ctx.lineWidth = 10;
-     ctx.strokeStyle = primaryColor || "black";
-     ctx.stroke();
-  
+    // Draw a center circle
+    ctx.beginPath()
+    ctx.arc(centerX, centerY, 50, 0, PI2, false)
+    ctx.closePath()
+    ctx.fillStyle = primaryColor || 'black'
+    ctx.lineWidth = 10
+    ctx.strokeStyle = contrastColor || 'white'
+    ctx.fill()
+    ctx.font = 'bold 1em proxima-nova'
+    ctx.fillStyle = contrastColor || 'white'
+    ctx.textAlign = 'center'
+    ctx.fillText(buttonText || 'Spin', centerX, centerY + 3)
+    ctx.stroke()
+
+    // Draw outer circle
+    ctx.beginPath()
+    ctx.arc(centerX, centerY, size, 0, PI2, false)
+    ctx.closePath()
+
+    ctx.lineWidth = 10
+    ctx.strokeStyle = primaryColor || 'black'
+    ctx.stroke()
   }
 
   const drawNeedle = () => {
-    let ctx = canvasContext
+    const ctx = canvasContext
     ctx.lineWidth = 1
     ctx.strokeStyle = contrastColor || 'white'
     ctx.fileStyle = contrastColor || 'white'
@@ -187,7 +181,7 @@ export const WheelComponent = ({
     ctx.lineTo(centerX, centerY - 70)
     ctx.closePath()
     ctx.fill()
-    let change = angleCurrent + Math.PI / 2
+    const change = angleCurrent + Math.PI / 2
     let i =
       segments.length -
       Math.floor((change / (Math.PI * 2)) * segments.length) -
@@ -195,13 +189,13 @@ export const WheelComponent = ({
     if (i < 0) i = i + segments.length
     ctx.textAlign = 'center'
     ctx.textBaseline = 'middle'
-    ctx.fillStyle = primaryColor || "black";
+    ctx.fillStyle = primaryColor || 'black'
     ctx.font = 'bold 1.5em proxima-nova'
-    current_segment = segments[i]
-    isStarted && ctx.fillText(current_segment, centerX + 10, centerY + size + 50)
+    currentSegment = segments[i]
+    isStarted && ctx.fillText(currentSegment, centerX + 10, centerY + size + 50)
   }
   const clear = () => {
-    let ctx = canvasContext
+    const ctx = canvasContext
     ctx.clearRect(0, 0, 1000, 800)
   }
   return (
@@ -213,7 +207,8 @@ export const WheelComponent = ({
         style={{
           pointerEvents: isFinished ? 'none' : 'auto'
         }}
-      ></canvas>
+      />
     </div>
   )
 }
+export default WheelComponent
